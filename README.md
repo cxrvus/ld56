290d7cb312c777efd8c689130eb6905d066d3b3a
Corvus <benito.m.rabe@gmail.com>
2024-10-05 00:05:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Bevy game template
+
+see the [original template](https://github.com/NiklasEi/bevy_game_template)
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bevy::asset::AssetMetaCheck;
+use bevy::prelude::*;
+use bevy::DefaultPlugins;
+
+mod game;
+use game::GamePlugin;
+
+fn main() {
+	App::new()
+		.insert_resource(Msaa::Off)
+		.insert_resource(ClearColor(Color::linear_rgb(0.4, 0.4, 0.4)))
+		.add_plugins(
+			DefaultPlugins
+				.set(WindowPlugin {
+					primary_window: Some(Window {
+						title: "Bevy game".to_string(), // todo
+						// Bind to canvas included in `index.html`
+						canvas: Some("#bevy".to_owned()),
+						fit_canvas_to_parent: true,
+						// Tells wasm not to override default event handling, like F5 and Ctrl+R
+						prevent_default_event_handling: false,
+						..default()
+					}),
+					..default()
+				})
+				.set(AssetPlugin {
+					meta_check: AssetMetaCheck::Never,
+					..default()
+				}),
+		)
+		.add_plugins(GamePlugin)
+		.run();
+}
