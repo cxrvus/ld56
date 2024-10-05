6abbfc283ec71f6758d5db453852338bc73d54d3
cxrvus <benito.m.rabe@gmail.com>
2024-10-05 16:48:00 +0200

    changed game name to LICEMIND

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 			DefaultPlugins
 				.set(WindowPlugin {
 					primary_window: Some(Window {
-						title: "Bevy game".to_string(), // todo
+						title: "LICEMIND".to_string(),
 						// Bind to canvas included in `index.html`
 						canvas: Some("#bevy".to_owned()),
 						fit_canvas_to_parent: true,
